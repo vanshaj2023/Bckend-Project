69467b74124e1444a40fb6ccc4e256a4eb26ead8
vanshaj2023 <kumar.vanshaj2023@gmail.com>
2024-07-09 21:35:50 +0530

    utilscode

--- src/app.js
+++ src/app.js
@@ -9,13 +9,13 @@ app.use(cors({
     credential: true
 }))
 
-app.use(express.json({limit : "16kb"}))
+app.use(express.json({ limit: "16kb" }))
 
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 
 app.use(express.static("public"))
 
 app.use(cookieParser())
 
 
-export {app}
+export { app }
